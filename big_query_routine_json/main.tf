199eb2ec9c2cb46ae8eb00f366966baba575a1cf
ewibrahim <elwalid.ibr@gmail.com>
2022-03-06 22:35:15 +0000

    2nd commit

--- big_query_routine_json/main.tf
+++ big_query_routine_json/main.tf
@@ -1,21 +1,33 @@
-resource "google_bigquery_dataset" "test" {
-	dataset_id = "dataset_id-${local.name_suffix}"
+resource "google_storage_bucket" "my_bucketcloud" {
+  name          = "west-bucket-cloud"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
 }
 
-resource "google_bigquery_routine" "sproc" {
-  dataset_id = google_bigquery_dataset.test.dataset_id
-  routine_id     = "tf_test_routine_id%{random_suffix}"
-  routine_type = "SCALAR_FUNCTION"
-  language = "JAVASCRIPT"
-  definition_body = "CREATE FUNCTION multiplyInputs return x*y;"
-  arguments {
-    name = "x"
-    data_type = "{\"typeKind\" :  \"FLOAT64\"}"
-  } 
-  arguments {
-    name = "y"
-    data_type = "{\"typeKind\" :  \"FLOAT64\"}"
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "insta2"
+  friendly_name               = "proto"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
   }
-   
-  return_type = "{\"typeKind\" :  \"FLOAT64\"}"
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
 }
